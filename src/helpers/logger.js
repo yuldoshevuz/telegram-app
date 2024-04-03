890e510db439d205f7e241280db007712b6e1e12
Muhammadali <mukhammadaliweb@gmail.com>
2024-04-03 11:00:40 +0500

    Added errorTime to logger.js

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const fs = require('fs')
+const moment = require('moment')
 
 function logger(error) {
     const errorMessage = (error && error.stack) ? error.stack : JSON.stringify(error)
+    const errorTime = moment(new Date()).format('MMMM Do YYYY, HH:mm:ss')
 
-    fs.appendFileSync(path.join(process.cwd(), 'error.log'), errorMessage + '\n', (err) => {
+    fs.appendFileSync(path.join(process.cwd(), 'error.log'), errorTime + errorMessage + '\n', (err) => {
         if (err) {
             fs.writeFileSync(path.join(process.cwd(), 'error.log'), errorMessage + '\n', (err) => {
                 if (err) {
